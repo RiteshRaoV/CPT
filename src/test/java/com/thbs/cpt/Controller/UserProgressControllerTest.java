87a67da9daeda28b4ab592398b8a1169acc2f98a
Ritesh Rao V <ritheshraov016@gmail.com>
2024-04-08 21:56:16 +0530

    bug fix

--- src/test/java/com/thbs/cpt/Controller/UserProgressControllerTest.java
+++ src/test/java/com/thbs/cpt/Controller/UserProgressControllerTest.java
@@ -6,7 +6,6 @@ import static org.mockito.Mockito.when;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
